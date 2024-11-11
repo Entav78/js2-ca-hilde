4dfeb3e99325623c3b37f8e9e9c4b4c2332e8ed3
Entav78 <152163293+Entav78@users.noreply.github.com>
2024-11-11 13:03:07 +0100

    login works (I think)

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,4 +1,17 @@
+/*export function authGuard() {
+  if (!localStorage.token) {
+    alert("You must be logged in to view this page");
+    window.location.href = "/auth/login/";
+  }
+}
+*/
 export function authGuard() {
+  const publicPaths = ["/auth/login/", "/auth/register/"];
+  
+  // Skip guard for public paths
+  if (publicPaths.includes(window.location.pathname)) return;
+
+  // Redirect if not logged in
   if (!localStorage.token) {
     alert("You must be logged in to view this page");
     window.location.href = "/auth/login/";
