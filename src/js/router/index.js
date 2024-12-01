871f0a58060a17bb773eb9c7940e20198851a7b0
Entav78 <152163293+Entav78@users.noreply.github.com>
2024-12-01 23:21:18 +0100

    fix last commit

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -11,31 +11,31 @@ export default async function router(pathname = window.location.pathname) {
     switch (cleanPathname) {
       case "/":
       console.log("Loading home view");
-      await import("../views/home.js");
+      await import("./views/home.js");
       break;
     case "/auth/":
-      await import("../views/auth.js");
+      await import("./views/auth.js");
       break;
     case "/auth/login/":
-      await import("../views/login.js");
+      await import("./views/login.js");
       break;
     case "/auth/register/":
-      await import("../views/register.js");
+      await import("./views/register.js");
       break;
     case "/post/":
-      await import("../views/post.js");
+      await import("./views/post.js");
       break;    
     case "/post/edit/":
-      await import("../views/postEdit.js");
+      await import("./views/postEdit.js");
       break;
     case "/post/manage/":
-      await import("../views/postManage.js");
+      await import("./views/postManage.js");
       break;  
     case "/profile/":
-      await import("../views/profile.js");
+      await import("./views/profile.js");
       break;
     default:
-      await import("../views/notFound.js");
+      await import("./views/notFound.js");
     }
   } catch (error) {
     console.error("Router Error:", error.message);
