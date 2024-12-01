a7364efb240bbf1058652b7a4185dd13cd9681bb
Entav78 <152163293+Entav78@users.noreply.github.com>
2024-12-01 13:50:57 +0100

    testing router/index paths

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,61 +1,4 @@
-// This function controls which JavaScript file is loaded on which page
-// In order to add additional pages, you will need to implement them below
-// You may change the behaviour or approach of this file if you choose
-
-/*const basePath = "/js2-ca-hilde"; // GitHub Pages base path
-
-const cleanPathname = pathname.replace(basePath, "") || "/";
-
-export default async function router(pathname = window.location.pathname) {
-console.log("Router running");
-  switch (pathname) {
-    case "/":
-      console.log("Loading home view");
-      await import("/views/home.js");
-      break;
-    case "/auth/":
-      await import("../views/auth.js");
-      break;
-    case "/auth/login/":
-      await import("../views/login.js");
-      break;
-    case "/auth/register/":
-      await import("../views/register.js");
-      break;
-    case "/post/":
-      await import("../views/post.js");
-      break;    
-    case "/post/edit/":
-      await import("../views/postEdit.js");
-      break;
-    case "/post/manage/":
-      await import("../views/postManage.js");
-      break;  
-    case "/profile/":
-      await import("../views/profile.js");
-      break;
-    default:
-      await import("../views/notFound.js");
-  }
-}
-*/
-
-/*
-export default async function router(pathname = window.location.pathname) {
-  console.log("Router running");
-  
-  try {
-    if (pathname === "/") {
-      console.log("Loading home view");
-      await import("../views/home.js");
-    } else {
-      console.log("Route not found");
-    }
-  } catch (error) {
-    console.error("Router Error:", error.message);
-  }
-}
-*/
+/* comment out for testing
 import { basePath } from "../api/constants.js";
 
 export default async function router(pathname = window.location.pathname) {
@@ -93,6 +36,50 @@ export default async function router(pathname = window.location.pathname) {
       break;
     default:
       await import("./views/notFound.js");
+    }
+  } catch (error) {
+    console.error("Router Error:", error.message);
+  }
+}
+*/
+
+import { basePath } from "../api/constants.js";
+
+export default async function router(pathname = window.location.pathname) {
+  console.log("Router running");
+
+  const cleanPathname = pathname.replace(basePath, "") || "/";
+  console.log("Clean Pathname:", cleanPathname);
+
+  try {
+    switch (cleanPathname) {
+      case "/":
+        console.log("Loading home view");
+        await import(`${basePath}/src/js/router/views/home.js`);
+        break;
+      case "/auth/":
+        await import(`${basePath}/src/js/router/views/auth.js`);
+        break;
+      case "/auth/login/":
+        await import(`${basePath}/src/js/router/views/login.js`);
+        break;
+      case "/auth/register/":
+        await import(`${basePath}/src/js/router/views/register.js`);
+        break;
+      case "/post/":
+        await import(`${basePath}/src/js/router/views/post.js`);
+        break;    
+      case "/post/edit/":
+        await import(`${basePath}/src/js/router/views/postEdit.js`);
+        break;
+      case "/post/manage/":
+        await import(`${basePath}/src/js/router/views/postManage.js`);
+        break;  
+      case "/profile/":
+        await import(`${basePath}/src/js/router/views/profile.js`);
+        break;
+      default:
+        await import(`${basePath}/src/js/router/views/notFound.js`);
     }
   } catch (error) {
     console.error("Router Error:", error.message);
