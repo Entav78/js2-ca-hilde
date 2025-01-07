d807d0c30dcfec24c968e35db128ae51ba745f78
Entav78 <152163293+Entav78@users.noreply.github.com>
2025-01-07 11:28:09 +0100

    Updated login functionality and links for deployment

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -27,24 +27,20 @@ export function headers(token = null) {
   return headers;
 }
 */
-import { API_KEY } from "./constants.js";
+import { API_KEY } from './constants.js';
 
-export function headers(token = localStorage.getItem("token")) {
+export function headers(token = null) {
   const headers = new Headers();
 
-  headers.append("Content-Type", "application/json");
+  headers.append('Content-Type', 'application/json');
 
   if (API_KEY) {
-    headers.append("X-Noroff-API-Key", API_KEY);
+    headers.append('X-Noroff-API-Key', API_KEY);
   }
 
   if (token) {
-    headers.append("Authorization", `Bearer ${token}`);
+    headers.append('Authorization', `Bearer ${token}`);
   }
 
   return headers;
 }
-
-
-
-
