e1a14ff63a337436e535eef6d78981e926b03067
Entav78 <152163293+Entav78@users.noreply.github.com>
2025-01-08 11:50:44 +0100

    test api

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,37 +1,14 @@
-/*import { API_KEY } from "./constants.js";
-
-export function headers() {
-  const headers = new Headers();
-
-  headers.append("Content-Type", "application/json");
-
-  if (API_KEY) {
-    headers.append("X-Noroff-API-Key", API_KEY);
-  }
-
-  return headers;
-}
-Testing new headers with bearer token*/
-
-/*another test
-export function headers(token = null) {
-  const headers = new Headers();
-
-  headers.append("Content-Type", "application/json");
-  headers.append("X-Noroff-API-Key", import.meta.env.VITE_API_KEY); // Use the API key
-
-  if (token) {
-    headers.append("Authorization", `Bearer ${token}`); // Add token only when provided
-  }
-
-  return headers;
-}
-*/
 import { API_KEY } from './constants.js';
 
 export function headers(accessToken = localStorage.getItem('accessToken')) {
   const headers = new Headers();
   headers.append('Content-Type', 'application/json');
+
+  console.log('Constructed Headers:', {
+    'Content-Type': headers.get('Content-Type'),
+    'X-Noroff-API-Key': headers.get('X-Noroff-API-Key'),
+    Authorization: headers.get('Authorization'),
+  });
 
   // Include API Key in headers
   if (API_KEY) {
