4a8af288c04cfd1b802612f8f4531e55aa097c49
Entav78 <152163293+Entav78@users.noreply.github.com>
2025-01-14 14:45:44 +0100

    Fix relative paths for scripts and assets

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  appType: "mpa",
-  base: "/js2-ca-hilde/", // Set the base path for GitHub Pages
+  appType: 'mpa',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/js2-ca-hilde/', // Dynamically set the base path
   build: {
-    target: "esnext",
+    target: 'esnext',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./index.html"),
-        login: resolve(__dirname, "./auth/login/index.html"),
-        auth: resolve(__dirname, "./auth/index.html"),
-        register: resolve(__dirname, "./auth/register/index.html"),
-        profile: resolve(__dirname, "./profile/index.html"),
-        post: resolve(__dirname, "./post/index.html"),
-        editPost: resolve(__dirname, "./post/edit/index.html"),
-        createPost: resolve(__dirname, "./post/manage/index.html"),
+        main: resolve(__dirname, './index.html'),
+        login: resolve(__dirname, './auth/login/index.html'),
+        auth: resolve(__dirname, './auth/index.html'),
+        register: resolve(__dirname, './auth/register/index.html'),
+        profile: resolve(__dirname, './profile/index.html'),
+        post: resolve(__dirname, './post/index.html'),
+        editPost: resolve(__dirname, './post/edit/index.html'),
+        createPost: resolve(__dirname, './post/manage/index.html'),
       },
     },
   },
   test: {
-    environment: "jsdom", // Use jsdom for DOM testing
+    environment: 'jsdom', // Use jsdom for DOM testing
   },
 });
