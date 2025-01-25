6fc77da65a9fc9cda63abf7a707b0f9f9727790b
Entav78 <152163293+Entav78@users.noreply.github.com>
2025-01-26 00:26:48 +0100

    Dist cleaned, site refreshed, and my sanity hanging by a thread.

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig({
   appType: 'mpa', // Multi-Page Application
   base: process.env.NODE_ENV === 'development' ? '/' : '/js2-ca-hilde/', // Base path for development and production
   build: {
-    target: 'esnext', // Modern build target
+    target: 'esnext',
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: resolve(__dirname, './index.html'),
@@ -23,13 +25,23 @@ export default defineConfig({
         editPost: resolve(__dirname, './post/edit/index.html'),
         createPost: resolve(__dirname, './post/manage/index.html'),
       },
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: ({ name }) => {
+          if (/\.(css)$/.test(name ?? '')) {
+            return 'css/[name]-[hash][extname]'; // CSS files in dist/css
+          }
+          return 'assets/[name]-[hash][extname]'; // Other assets
+        },
+      },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use 'sass:color';`, // Inject global SCSS usage
-        includePaths: [path.resolve(__dirname, 'src/scss')], // Include SCSS paths
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use 'sass:color';`, // Inject global SCSS usage
+          includePaths: [path.resolve(__dirname, 'src/scss')], // Include SCSS paths
+        },
       },
     },
   },
