89978b55914b9a174aebe788ac5350c95c234cb5
Entav78 <152163293+Entav78@users.noreply.github.com>
2025-01-26 16:37:40 +0100

    Cleaned up gh-pages branch and deployed dist files

--- src/js/router/views/register.js
+++
@@ -1,8 +0,0 @@
-import { Register } from "../../api/auth/register.js";
-import { API_AUTH_REGISTER } from "../../api/constants.js";
-
-const registerInstance = new Register(API_AUTH_REGISTER); 
-const form = document.forms.register;
-
-form.addEventListener("submit", (event) => registerInstance.handleRegister(event));
-
--- src/js/ui/post/update.js
+++
@@ -1,4 +0,0 @@
-/**
- * Passses data to the createPost function in api/post and handles the response
- */
-export async function onUpdatePost(event) {}
